3ca2b4fed895e568e679c107f29c15df6ee83bc0
TSRSE <26186238+TSRSE@users.noreply.github.com>
2023-11-28 15:07:18 +0300

    Add condition check for the grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,25 @@
-const gameStateManager = (function(){
+const getKeyByValue = (object, value) => Object.keys(object).find(key => object[key] === value);
 
-    // const getElementByValue = (object, value) => {return object.find(x => x.)}
+const gameStateManager = (function(){
+    let grid = []
 
     let currentStage = 0
-    const gameStages = {none : 0, start : 1, playing : 2, ended: 3}
+    const gameStages = { 0 : 'none' , 1 : 'start', 2 : 'playing', 3 : 'ended' }
     const setStage = (newState) => { currentStage = gameStages[newState]; console.log(currentStage) }
     const getCurrentStage = () => currentStage
     
-
     let turnState = 0
-    const turnStates = {X : 0, O : 1}
-    const setTurn = (newState) => { turnState = turnStates[newState]; console.log(turnState) }
+    const turnStates = { 0 : 'X', 1 : 'O' }
+    const toggleTurn = () => { turnState = turnState == 0 ? 1 : 0; }
     const getCurrentTurn = () => { console.log(turnStates[turnState]); return turnStates[turnState]; }
+
+    function startGame(){
+        setStage(2);
+        grid = gridController.createGrid()
+        gridController.drawGrid(grid)
+    }
     
-    return {setTurn, getCurrentTurn, setStage, getCurrentStage}
+    return {getCurrentTurn, setStage, getCurrentStage, toggleTurn, startGame}
 })()
 
 function testStateManager(){
@@ -21,43 +27,74 @@ function testStateManager(){
     console.log(`${gameStateManager.getCurrentStage()}`)
 }
 
-function Cell(cell, index){
-    this.index = index
-    this.cell = cell
-}
+const gridController = (function(){
+    function Cell(){
+        this.states = { null: 'empty', 0 : 'X', 1 : 'O' }
+        this.state = this.states['empty']
+    
+        this.setState = (newState) => { 
+            this.state = this.states[newState]; this.updateText()
+            gameStateManager.toggleTurn()
+        }
+    
+        this.asDomElement = document.createElement('div')
+        this.asDomElement.className = 'cell'
+        this.asDomElement.onclick = () => {
+            if (this.state != this.states['empty']) { return; }
+            if (gameStateManager.getCurrentStage() != 'playing') { return; }
+            
+            this.setState(getKeyByValue(this.states, gameStateManager.getCurrentTurn()));
+            checkGridForCondition(gameStateManager.grid);
+        }
+    
+        this.updateText = () => { this.asDomElement.innerText = this.state;}
+    }
 
-const cell = (function(){
-    const states = {empty: null, X : 0, O : 1}
-    let state = states['empty']
 
-    const setState = (newState) => { state = states[newState]; updateText()}
+    function createGrid(){
+        let _array = [];
+        for (let index = 0; index < 9; index++) 
+        { _array.push(new gridController.Cell()); }
+        return _array;
+    }
+    
+    function drawGrid(array){
+        const gridUIElement = document.getElementById('grid')
+        gridUIElement.innerHTML = ''
+        for (let index = 0; index < array.length; index++) {
+            let linkedCell = array[index]['asDomElement']; linkedCell.setAttribute('id', index);
+            gridUIElement.appendChild(linkedCell)
+        }
+    }
 
-    const asDomElement = document.createElement('div')
-    asDomElement.className = 'cell'
-    asDomElement.onclick = () => {asDomElement.innerText = 'clicked!'}
+    function checkGridForCondition(array){
+        for (let index = 0; index <= 6; index += 3) {
+            if (array[index].state == array[index+1].state && array[index].state == array[index+2].state && array[index].state != null) {
+                alert('SAME Horizontal!')
+                gameStateManager.setStage(3);
+            }
+        }
 
-    // const updateText = () => innerText = state == states['X'] ? 'O' : 'X';
+        for (let index = 0; index < 3; index++) {
+            if (array[index].state == array[index+3].state && array[index].state == array[index+6].state && array[index].state != null) {
+                alert('SAME Vertical!')
+                gameStateManager.setStage(3);
+            }
+        }
 
-    return { setState, state, asDomElement}
+        if (array[0].state == array[4].state && array[0].state == array[8].state && array[0].state != null) {
+            alert('Left to right')
+            gameStateManager.setStage(3);
+        }
+        
+        if (array[2].state == array[4].state && array[2].state == array[6].state && array[2].state != null) {
+            alert('Right to left')
+            gameStateManager.setStage(3);
+        }
+    }
+
+    return { Cell, createGrid, drawGrid }
 })()
 
-function createGrid(array){
-    for (let index = 0; index < 9; index++) 
-    { array.push(new Cell(cell, index)); }
-    // console.log(array)
-    // console.log(grid)
-}
 
-function drawGrid(array){
-    let gridUIElement = document.getElementById('grid')
-    gridUIElement.innerHTML = ''
-    for (let index = 0; index < array.length; index++) {
-        gridUIElement.cloneNode(array[index]['cell']['asDomElement'])
-    }
-}
-
-const grid = []
-createGrid(grid)
-drawGrid(grid)
-
-// testStateManager();
+gameStateManager.startGame()
